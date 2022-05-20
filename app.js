959db95b1f8067100845be419c00807bb9dc343c
sazzadhossain1 <sazzadtomal1@gmail.com>
2022-05-20 14:22:38 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,9 @@
+const nameAndAge = ["I am Sazzad Hossain and 25. "];
+const profession = ["I am a web developer. "];
+const passion = ['I love "Javascript"'];
+
+const addReference = nameAndAge + profession + passion;
+console.log(addReference);
+
+const totalReference = addReference;
+console.log(totalReference);
